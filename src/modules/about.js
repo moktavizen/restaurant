9980135db848e17d03da2ae7067bce23c3cf1552
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-02-12 20:00:41 +0700

    Add restaurant address to About page

--- src/modules/about.js
+++ src/modules/about.js
@@ -7,17 +7,30 @@ function createAboutComponent() {
 
   const headingDiv = createHeadingComponent("About");
 
+  const aboutContainerDiv = document.createElement("div");
+  aboutContainerDiv.classList = "about-container";
+
   const aboutP = document.createElement("p");
-  aboutP.classList = "about-paragraph";
-  aboutP.textContent = `Welcome to our kitchen, where we bring the authentic,
-bold flavors of Padang cuisine straight to your table. Whether
+  aboutP.textContent = `Welcome to our kitchen. Whether
 you are craving the melt-in-your-mouth tenderness of our slow-cooked Rendang,
 the signature crunch of a Barendo Egg, or the unique taste of Ayam Pop, our
-mission is to provide a heartwarming dining experience that celebrates the
-vibrant heritage of Minangkabau culture.`;
+mission is to provide a heartwarming experience that celebrates the
+vibrant heritage of Padang and Minangkabau.`;
+
+  const addressDiv = document.createElement("div");
+  addressDiv.textContent = "Address: ";
+
+  const addressA = document.createElement("a");
+  addressA.setAttribute("target", "_blank");
+  addressA.setAttribute("href", "https://maps.app.goo.gl/1GdaFpXUyh2M2toF9");
+  addressA.textContent = "112 Veteran St., Lamongan, Indonesia";
+
+  aboutContainerDiv.appendChild(aboutP);
+  aboutContainerDiv.appendChild(addressDiv);
+  aboutContainerDiv.appendChild(addressA);
 
   aboutDiv.appendChild(headingDiv);
-  aboutDiv.appendChild(aboutP);
+  aboutDiv.appendChild(aboutContainerDiv);
 
   return aboutDiv;
 }
